bd04187dbba793719ae7f3a4d9317f7ea412a92c
dlimdlim <dlim7272@gmail.com>
2022-07-31 16:44:09 +0900

    refactor code

--- VulkanPractice/main.cpp
+++ VulkanPractice/main.cpp
@@ -517,19 +517,17 @@ private:
             createInfo.image = swapChainImages[i];
             createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
             createInfo.format = swapChainImageFormat;
-
             createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
             createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
             createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
             createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
-
             createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
             createInfo.subresourceRange.baseMipLevel = 0;
             createInfo.subresourceRange.levelCount = 1;
             createInfo.subresourceRange.baseArrayLayer = 0;
             createInfo.subresourceRange.layerCount = 1;
 
-            vk_check(vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]));
+            vk_check((vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]));
         }
 
     }
