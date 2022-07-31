c630cc5184c822e33d9ed3a55a971f9d36574d70
dlimdlim <dlim7272@gmail.com>
2022-08-01 00:06:13 +0900

    allow window resize

--- VulkanPractice/main.cpp
+++ VulkanPractice/main.cpp
@@ -129,7 +129,7 @@ private:
     void initWindow() {
         glfwInit();
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        // glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
